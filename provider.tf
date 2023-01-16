bf17cec4b3e426a73b8c746e0ebc23d75cc23e6d
Mohamed El-Zakazeky <mohamed.mamdouh1510@gmail.com>
2023-01-16 12:51:49 +0200

    update

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-
-    shared_config_files = ["/home/mmamdouh/.aws/config"]
-    shared_credentials_files = ["/home/mmamdouh/.aws/credentials"]
-    profile = "terraform_id"
-
-}
